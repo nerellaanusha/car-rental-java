a000f42ac872c9816aa25b1144392a22d370d8c6
anusha <anusha1113@gmail.com>
2019-11-21 23:13:55 -0600

    token values updated

--- src/main/java/com/carrental/security/jwt/JwtProvider.java
+++ src/main/java/com/carrental/security/jwt/JwtProvider.java
@@ -16,10 +16,10 @@ public class JwtProvider {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtProvider.class);
 
-    @Value("${grokonez.app.jwtSecret}")
+    @Value("${carrental.app.jwtSecret}")
     private String jwtSecret;
 
-    @Value("${grokonez.app.jwtExpiration}")
+    @Value("${carrental.app.jwtExpiration}")
     private int jwtExpiration;
 
     public String generateJwtToken(Authentication authentication) {
