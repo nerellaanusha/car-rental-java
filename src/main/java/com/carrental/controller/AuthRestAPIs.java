ecf2df84201812bb08046d7873edc74ebd1d9a2a
anusha <anusha1113@gmail.com>
2019-12-04 21:14:37 -0600

    deleted unused code

--- src/main/java/com/carrental/controller/AuthRestAPIs.java
+++ src/main/java/com/carrental/controller/AuthRestAPIs.java
@@ -91,38 +91,13 @@ public class AuthRestAPIs {
 					HttpStatus.CONFLICT);
 		}
 
-		// Creating user's account
+	
 		User user = new User(signUpRequest.getFirstName(), signUpRequest.getLastName(),signUpRequest.getUserName(),
 				signUpRequest.getEmail(),encoder.encode(signUpRequest.getPassword()));
 		
 		user.setId(nextSequenceService.getNextSequence(User.SEQUENCE_NAME));
 		user.setRole("ROLE_USER");
 
-//		Set<String> strRoles = signUpRequest.getRole();
-//		Set<Role> roles = new HashSet<>();
-//
-//		strRoles.forEach(role -> {
-//			switch (role) {
-//			case "admin":
-//				Role adminRole = roleRepository.findByName(RoleName.ROLE_ADMIN)
-//						.orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
-//				roles.add(adminRole);
-//
-//				break;
-//			case "pm":
-//				Role pmRole = roleRepository.findByName(RoleName.ROLE_PM)
-//						.orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
-//				roles.add(pmRole);
-//
-//				break;
-//			default:
-//				Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
-//						.orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
-//				roles.add(userRole);
-//			}
-//		});
-
-		//user.setRoles(roles);
 		userRepository.save(user);
 
 		
